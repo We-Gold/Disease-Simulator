8c58de43847ce06300c99095bfd0d35f3297fb31
W G <wegosci@gmail.com>
2021-01-02 16:30:30 +0000

    Added the basic user interface and some basic js functionality.

---
+++ script.js
@@ -0,0 +1,67 @@
+// Create a model for storing configuration data
+params = {
+  "map": "Town",
+  "disease": "Influenza A",
+  "initialPopulation": 1000,
+  "masks": true,
+  // "maskDelay": 0,
+  // "maskPercent": 90,
+  "quarantine": true,
+  // "quarDelay": 0,
+  // "quarPercent": 95,
+  "vaccine": true,
+  // "vaccDelay": 300,
+  "vaccEffectiveness": 85.63,
+  "medCareCapacity": 135
+}
+
+let isRunning = false
+
+let show = (el) => {
+  el.style.display = "block"
+}
+
+let hide = (el) => {
+  el.style.display = "none"
+}
+
+document.getElementById("beginSimButton").addEventListener('click', ()=>{
+  for(var key of Object.keys(params)) {
+    params[key] = ["masks", "quarantine", "vaccine"].includes(key) ? document.getElementById(key).checked : document.getElementById(key).value
+  }
+  isRunning = true
+  hide(document.getElementById("beginSimButton"))
+  show(document.querySelector(".btn-group"))
+})
+
+document.getElementById("toggleSimButton").addEventListener('click', ()=>{
+  isRunning = !isRunning;
+  document.getElementById("toggleSimButton").innerText == "Resume" ? document.getElementById("toggleSimButton").innerText = "Pause" : document.getElementById("toggleSimButton").innerText = "Resume"
+})
+
+document.getElementById("endSimButton").addEventListener('click', ()=>{
+  isRunning = false;
+  // TODO do other stuff here to end the simulation
+  show(document.getElementById("beginSimButton"))
+  hide(document.querySelector(".btn-group"))
+})
+
+// TODO Create a model for storing data about the diseases
+
+// Define details about the sketch
+let simArea = document.getElementById('sim-area')
+
+sk = {
+  w: simArea.clientWidth,
+  h: simArea.clientHeight
+}
+
+// Instantiate and define the sketch
+let sketch = p => {
+  p.setup = () => {
+    p.createCanvas(sk.w, sk.h)
+    p.background('#C4C4C4')
+  }
+}
+
+new p5(sketch, 'sketch-container')
